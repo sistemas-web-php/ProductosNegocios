45a96ee0e83dcf06fe6d085242a7cb5df9c7950d
Rodrigo Gabriel Reynoso <50190273+rodrigo402@users.noreply.github.com>
2019-09-15 20:25:43 -0300

    ..

--- vista/principal-vista.php
+++ vista/principal-vista.php
@@ -6,7 +6,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Filtrando elementos por categorias</title>
+	<title>Catalogo - Productos</title>
 
 	<link rel="stylesheet" href="css/estilos.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
@@ -32,34 +32,66 @@
 				<div class="product-item" category="laptops">
 					<img src="vista/imagenes/laptop_hp.jpg" alt="" >
 					<a href="#">Laptop Hp</a>
+					<p>DESCRIPCION 1</p>
+					<P>ADASDASDASASD 2</P>
+					<P>ADASDASDASASD 3</P>
+					<P>ADASDASDASASD 4</P>
 				</div>
 				<div class="product-item" category="laptops">
 					<img src="vista/imagenes/laptop_toshiba.jpg" alt="" >
 					<a href="#">Laptop Toshiba</a>
+					<p>DESCRIPCION 1</p>
+					<P>ADASDASDASASD 2</P>
+					<P>ADASDASDASASD 3</P>
+					<P>ADASDASDASASD 4</P>
 				</div>
 				<div class="product-item" category="smartphones">
 					<img src="vista/imagenes/samsung_galaxy.jpg" alt="" >
 					<a href="#">Samsung Galaxy</a>
+					<p>DESCRIPCION 1</p>
+					<P>ADASDASDASASD 2</P>
+					<P>ADASDASDASASD 3</P>
+					<P>ADASDASDASASD 4</P>
 				</div>
 				<div class="product-item" category="smartphones">
 					<img src="vista/imagenes/iphone.jpg" alt="" >
 					<a href="#">Iphone 7</a>
+					<p>DESCRIPCION 1</p>
+					<P>ADASDASDASASD 2</P>
+					<P>ADASDASDASASD 3</P>
+					<P>ADASDASDASASD 4</P>
 				</div>
 				<div class="product-item" category="ordenadores">
 					<img src="vista/imagenes/pc_hp.jpg" alt="" >
 					<a href="#">PC Hp</a>
+					<p>DESCRIPCION 1</p>
+					<P>ADASDASDASASD 2</P>
+					<P>ADASDASDASASD 3</P>
+					<P>ADASDASDASASD 4</P>
 				</div>
 				<div class="product-item" category="ordenadores">
 					<img src="vista/imagenes/pc_lenovo.jpg" alt="" >
 					<a href="#">PC Lenovo</a>
+					<p>DESCRIPCION 1</p>
+					<P>ADASDASDASASD 2</P>
+					<P>ADASDASDASASD 3</P>
+					<P>ADASDASDASASD 4</P>
 				</div>
 				<div class="product-item" category="monitores">
 					<img src="vista/imagenes/monitor_asus.jpg" alt="" >
 					<a href="#">Monitor Asus</a>
+					<p>DESCRIPCION 1</p>
+					<P>ADASDASDASASD 2</P>
+					<P>ADASDASDASASD 3</P>
+					<P>ADASDASDASASD 4</P>
 				</div>
 				<div class="product-item" category="audifonos">
 					<img src="vista/imagenes/jbl.jpg" alt="" >
 					<a href="#">Audifonos JBL</a>
+					<p>DESCRIPCION 1</p>
+					<P>ADASDASDASASD 2</P>
+					<P>ADASDASDASASD 3</P>
+					<P>ADASDASDASASD 4</P>
 				</div>
 			</section>
 		</div>
