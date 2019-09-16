f607773a7448b4c8e2ee86c6957d29013db04a59
Rodrigo Gabriel Reynoso <50190273+rodrigo402@users.noreply.github.com>
2019-09-16 00:42:59 -0300

    sdasdas

--- admin/vista/head.php
+++ admin/vista/head.php
@@ -4,15 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Universal Cine</title>
+    <title>Bienvenido - Admin</title>
     <link rel="stylesheet" href="<?php echo RAIZ . 'vista/css/bootstrap.css'; ?>">
-    
-    <script src="<?php echo RAIZ . 'vista/js/bootstrap.js'; ?>"></script>
-    <script src="<?php echo RAIZ . 'vista/js/jquery.js'; ?>"></script>
-
-    <link rel="stylesheet" href="<?php echo RAIZ . 'vista/slick/slick.css'; ?>">
-    <link rel="stylesheet" href="<?php echo RAIZ . 'vista/slick/slick-theme.css'; ?>">
+   
 
     <link rel="stylesheet" href="<?php echo RAIZ . 'vista/css/mis-estilos.css'; ?>">
 
 </head>
+<body>
+   
+
+</body>
--- admin/vista/navbar.php
+++ admin/vista/navbar.php
@@ -1,123 +1,41 @@
-<style>
-header {
-	width:100%;
-}
- 
-header nav {
-	width:100%;
-	background:#024959;
-}
- 
-.menu_bar {
-	display:none;
-}
- 
-header nav ul {
-	overflow:hidden;
-	list-style:none;
-}
- 
-header nav ul li {
-	float:left;
-}
- 
-header nav ul li a {
-	color:#fff;
-	padding:20px;
-	display:block;
-	text-decoration:none;
-}
- 
-header nav ul li span {
-	margin-right:10px;
-}
- 
-header nav ul li a:hover {
-	background:#037E8C;
-}
- 
-section {
-	padding:20px;
-}
- 
-@media screen and (max-width:800px ) {
-	header nav {
-		width:80%;
-		height:100%;
-		left:-100%;
-		margin:0;
-		position: fixed;
-	}
- 
-	header nav ul li {
-		display:block;
-		float:none;
-		border-bottom:1px solid rgba(255,255,255, .3);
-	}
- 
-	.menu_bar {
-		display:block;
-		width:100%;
-		background:#ccc;
-	}
- 
-	.menu_bar .bt-menu {
-		display:block;
-		padding:20px;
-		background:#024959;
-		color:#fff;
-		text-decoration:none;
-		font-weight: bold;
-		font-size:25px;
-		-webkit-box-sizing:border-box;
-		-moz-box-sizing:border-box;
-		box-sizing:border-box;
-	}
- 
-	.menu_bar span {
-		float:right;
-		font-size:40px;
-	}
-}
-</style>
-	<header>
-		<div class="menu_bar">
-			<a href="#" class="bt-menu"><span class="icon-list2"></span>Menu</a>
-		</div>
- 
-		<nav>
-			<ul>
-				<li><a href="#"><span class="icon-house"></span>Inicio</a></li>
-				<li><a href="#"><span class="icon-suitcase"></span>Trabajos</a></li>
-				<li><a href="#"><span class="icon-rocket"></span>Proyectos</a></li>
-				<li><a href="#"><span class="icon-earth"></span>Servicios</a></li>
-				<li><a href="#"><span class="icon-mail"></span>Contactos</a></li>
-			</ul>
-		</nav>
-	</header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bienvenido - Admin</title>
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+	<link rel="stylesheet" href="<?php echo RAIZ . 'vista/css/mis-estilos.css'; ?>">
+</head>
+<body>
 
-<script>
-$(document).ready(main);
- 
- var contador = 1;
-  
- function main(){
-   $('.menu_bar').click(function(){
-     // $('nav').toggle(); 
-  
-     if(contador == 1){
-       $('nav').animate({
-         left: '0'
-       });
-       contador = 0;
-     } else {
-       contador = 1;
-       $('nav').animate({
-         left: '-100%'
-       });
-     }
-  
-   });
-  
- };
-</script>
+<div class="wrapper">
+    <div class="sidebar">
+        <h2>Menu</h2>
+        <ul>
+            <li><a href="#"><i class="fas fa-home"></i>Productos</a></li>
+            <li><a href="#"><i class="fas fa-user"></i>Categorias</a></li>
+            <li><a href="#"><i class="fas fa-address-card"></i>Perfil</a></li>
+            <li><a href="#"><i class="fas fa-project-diagram"></i>Usuarios</a></li>
+            <li><a href="#"><i class="fas fa-blog"></i>Ajustes</a></li>
+        </ul> 
+        <div class="social_media">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+      </div>
+    </div>
+    <div class="main_content">
+        <div class="header"> <p>Bienvenido - Admin </p></div>  
+        <div class="info">
+          <div>En nombre de FANTASY, les damos la más sincera bienvenida a nuestra página web, en la que, esperamos, encontrarán toda la información que necesiten acerca de nuestras actividades.
+
+			Esta web quiere ser también testigo del crecimiento de FANTASY, desde sus orígenes hasta su incursión en aquellos sectores demandados por la sociedad.
+
+			Pero el objetivo principal de este canal de comunicación es plasmar los valores que nos respaldan: el compromiso social, la máxima calidad y la voluntad de servicio, así como nuestro interés por todas aquellas ventajas que nos ofrece la tecnología. Todo ello tiene una presencia destacada en esta página web y en nuestras propias decisiones.</div>
+         
+      </div>
+    </div>
+</div>
+
+</body>
+</html>
