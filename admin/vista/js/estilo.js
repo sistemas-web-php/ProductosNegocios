af7d127557d19fd325c1e1071932916253a88060
Rodrigo Gabriel Reynoso <50190273+rodrigo402@users.noreply.github.com>
2019-09-20 11:32:48 -0300

    sadsa
    asdsad

--- admin/vista/js/estilo.js
+++ admin/vista/js/estilo.js
@@ -1,30 +1,4 @@
-$(document).ready(function () {
-    var altoPantalla = $( window ).height();
+// SideNav Initialization
+$(".button-collapse").sideNav();
 
-    var altoNav = $('nav').height();
-    
-    var altoBanner = altoPantalla - altoNav;
-    
-    $('#banner').height(altoBanner);
-});
-
-function verPelicula(ver) {
-    window.location=ver;
-}
-
-function redEditarPel(id) { 
-    window.location='?view=modificacion-pelicula&id=' + id;
-}
-
-function buscar(){
-    $('#form-buscador').submit();
-}
-
-function botonLatino(){
-    $('#idioma-latino').removeClass('d-none');
-    $('#idioma-original').addClass('d-none');
-}
-function botonOriginal(){
-    $('#idioma-latino').addClass('d-none');
-    $('#idioma-original').removeClass('d-none');
-}
+new WOW().init();
