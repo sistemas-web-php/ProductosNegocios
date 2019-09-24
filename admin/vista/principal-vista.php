cc59b2e65af02191fd9db2d34719d08d5b429c10
Rodrigo Gabriel Reynoso <50190273+rodrigo402@users.noreply.github.com>
2019-09-24 11:55:18 -0300

    agrego el footer
    hay que maquetarlo bien

--- admin/vista/principal-vista.php
+++ admin/vista/principal-vista.php
@@ -9,37 +9,24 @@
         <h2>MENU</h2>
         <ul>
             <li><a href="#"><i class="fas fa-home"></i>INICIO</a></li>
-            <li><a href="#" onclick='desplegar(this);'><i class="fas fa-user"></i>PERFIL</a>
-            <ul>
-                <li class="submenu">item 1</li>
-                <li class="submenu">item 2</li>
-                <li class="submenu">item 3</li>
-              </ul>
-          
-          </li>
+            <li><a href="#"><i class="fas fa-user"></i>PERFIL</a></li>
             <li><a href="#"><i class="fas fa-address-card"></i>ACERCA DE</a></li>
             <li><a href="#"><i class="fas fa-project-diagram"></i>PORTAFOLIO</a></li>
             <li><a href="#"><i class="fas fa-blog"></i>BLOGS</a></li>
             <li><a href="#"><i class="fas fa-address-book"></i>CONTACTO</a></li>
-            <li><a href="#" onclick='desplegar();'><i class="fas fa-map-pin"></i>MAPS</a>
-          
-              <ul>
-                <li class="submenu">item 1</li>
-                <li class="submenu">item 2</li>
-                <li class="submenu">item 3</li>
-              </ul>
-          
-          </li>
-            <li><a href="#"><i class="fas fa-project-diagram"></i>PORTAFOLIO</a></li>
+            <li><a href="#"><i class="fas fa-map-pin"></i>MAPS</a></li>
         </ul> 
-
+        <div class="social_media">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+      </div>
     </div>
     
 </div>
 
-<?php include_once(VISTA . "footer.php") ?>
-
 </body>
 
+<?php include_once(VISTA . "footer.php") ?>
 
 </html>
