e5a0a2c17025993284b064398b26cc2f45621b5a
Tiago <tiagoriver33@gmail.com>
2019-09-23 22:41:42 -0300

    fd

--- admin/vista/principal-vista.php
+++ admin/vista/principal-vista.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <?php include_once(VISTA); ?>
-
 <?php include_once(VISTA . "head.php") ?>
 
-<body class="fixed-sn light-blue-skin">
-
+<body>
   <!--Double navigation-->
   <header>
     <!-- Sidebar navigation -->
@@ -138,147 +135,7 @@
     <!-- /.Navbar -->
   </header>
   <!--/.Double navigation-->
-
-  <!--Main layout-->
-  <main>
-
-    <div class="container-fluid text-center">
-
-      <!--Card-->
-      <div class="card card-cascade wider reverse my-4 pb-5">
-
-        <!--Card image-->
-        <div class="view view-cascade overlay wow fadeIn">
-          <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(137).jpg" class="img-fluid">
-          <a href="#!">
-            <div class="mask rgba-white-slight"></div>
-          </a>
-        </div>
-        <!--/Card image-->
-
-        <!--Card content-->
-        <div class="card-body card-body-cascade text-center wow fadeIn" data-wow-delay="0.2s">
-          <!--Title-->
-          <h4 class="card-title"><strong>My adventure</strong></h4>
-          <h5 class="blue-text"><strong>Photography</strong></h5>
-
-          <p class="card-text">Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
-            laudantium, totam rem aperiam.
-          </p>
-
-          <a class="btn btn-primary btn-lg">Primary button</a>
-          <a class="btn btn-secondary btn-lg">Secondary button</a>
-          <a class="btn btn-default btn-lg">Default button</a>
-
-        </div>
-        <!--/.Card content-->
-
-      </div>
-      <!--/.Card-->
-
-    </div>
-
-  </main>
-  <!--/Main layout-->
-
-  <!--Footer-->
-  <footer class="page-footer text-center text-md-left pt-4">
-
-    <!--Footer Links-->
-    <div class="container-fluid">
-      <div class="row">
-
-        <!--First column-->
-        <div class="col-md-3">
-          <h5 class="text-uppercase font-weight-bold mb-4">Footer Content</h5>
-          <p>Here you can use rows and columns here to organize your footer content.</p>
-        </div>
-        <!--/.First column-->
-
-        <hr class="w-100 clearfix d-md-none">
-
-        <!--Second column-->
-        <div class="col-md-2 mx-auto">
-          <h5 class="text-uppercase font-weight-bold mb-4">Links</h5>
-          <ul class="list-unstyled">
-            <li><a href="#!">Link 1</a></li>
-            <li><a href="#!">Link 2</a></li>
-            <li><a href="#!">Link 3</a></li>
-            <li><a href="#!">Link 4</a></li>
-          </ul>
-        </div>
-        <!--/.Second column-->
-
-        <hr class="w-100 clearfix d-md-none">
-
-        <!--Third column-->
-        <div class="col-md-2 mx-auto">
-          <h5 class="text-uppercase font-weight-bold mb-4">Links</h5>
-          <ul class="list-unstyled">
-            <li><a href="#!">Link 1</a></li>
-            <li><a href="#!">Link 2</a></li>
-            <li><a href="#!">Link 3</a></li>
-            <li><a href="#!">Link 4</a></li>
-          </ul>
-        </div>
-        <!--/.Third column-->
-
-        <hr class="w-100 clearfix d-md-none">
-
-        <!--Fourth column-->
-        <div class="col-md-2 mx-auto">
-          <h5 class="text-uppercase font-weight-bold mb-4">Links</h5>
-          <ul class="list-unstyled">
-            <li><a href="#!">Link 1</a></li>
-            <li><a href="#!">Link 2</a></li>
-            <li><a href="#!">Link 3</a></li>
-            <li><a href="#!">Link 4</a></li>
-          </ul>
-        </div>
-        <!--/.Fourth column-->
-
-      </div>
-    </div>
-    <!--/.Footer Links-->
-
-    <hr>
-
-    <!--Call to action-->
-    <div class="call-to-action text-center my-4">
-      <ul class="list-unstyled list-inline">
-        <li class="list-inline-item">
-          <h5>Register for free</h5>
-        </li>
-        <li class="list-inline-item"><a href="" class="btn btn-primary">Sign up!</a></li>
-      </ul>
-    </div>
-    <!--/.Call to action-->
-
-    <hr>
-
-    <!--Social buttons-->
-    <div class="social-section text-center">
-      <ul class="list-unstyled list-inline">
-        <li class="list-inline-item"><a class="btn-floating btn-fb"><i class="fab fa-facebook-f"> </i></a></li>
-        <li class="list-inline-item"><a class="btn-floating btn-tw"><i class="fab fa-twitter"> </i></a></li>
-        <li class="list-inline-item"><a class="btn-floating btn-gplus"><i class="fab fa-google-plus-g"> </i></a></li>
-        <li class="list-inline-item"><a class="btn-floating btn-li"><i class="fab fa-linkedin-in"> </i></a></li>
-        <li class="list-inline-item"><a class="btn-floating btn-git"><i class="fab fa-github"> </i></a></li>
-      </ul>
-    </div>
-    <!--/.Social buttons-->
-
-    <!--Copyright-->
-    <div class="footer-copyright py-3 text-center">
-      <div class="container-fluid">
-        © 2018 Copyright: <a href="http://www.MDBootstrap.com"> MDBootstrap.com </a>
-
-      </div>
-    </div>
-    <!--/.Copyright-->
-
-  </footer>
-  <!--/.Footer-->
+<?php include_once(VISTA . "footer.php"); ?>
 
 </body>
 
